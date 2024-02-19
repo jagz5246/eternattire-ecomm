a71e3052d2becf9438962754abd150e728e88974
Jagan G Mohan <jagz5246@gmail.com>
2024-02-19 15:22:05 +0530

    updated checkout page

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = async() => {
             <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
                 <h1 className='mb-4 text-4xl font-bold text-black sm:text-5xl mb:mb-8 md:text-6xl'>Top fashion for unbeatable price!</h1>
                 <p className='max-w-md leading-relaxed text-gray-500 xl:text-lg'>
-                    Welcome to ShopKrat, your ultimate destination for the latest trends in fashion and accessories!
+                    Welcome to eternAttire, your ultimate destination for the latest trends in fashion and accessories!
                 </p>
             </div>
         {/* Hero images */}
