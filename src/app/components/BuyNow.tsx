1daaca95b45d3dd3de95c047fb6694edb05f88fc
Jagan G Mohan <jagz5246@gmail.com>
2024-02-19 23:10:37 +0530

    updated footer

--- src/app/components/BuyNow.tsx
+++ src/app/components/BuyNow.tsx
@@ -5,19 +5,15 @@ import { ProductCart } from './AddToBag'
 import { Button } from '@/components/ui/button'
 import { useShoppingCart } from 'use-shopping-cart'
 import { urlFor } from '../lib/sanity';
-import { redirect } from 'next/navigation';
 
 
 const BuyNow = ({name, description, price, currency, image, price_id}: ProductCart) => {
-  const {addItem, cartDetails} = useShoppingCart()
+  const {addItem} = useShoppingCart()
   const {push} = useRouter()
 
   const buyNow = (price_id: string) =>{
     addItem(product)
-<<<<<<< HEAD
-=======
     // push("http://localhost:3000/pages/CheckoutPage")
->>>>>>> a96788a (updated footer)
     push("https://eternattire-ecomm.vercel.app/pages/CheckoutPage")
   }
 
